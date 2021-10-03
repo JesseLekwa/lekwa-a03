3522b01949615ef6cb098896069d22265617aa56
JesseLekwa <jesse.lekwa@gmail.com>
2021-10-03 15:25:10 -0400

    exercise 27 final commit

---
+++ exercise27/src/test/java/Solution27test.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class solution27test {
+    @Test
+    void validateFirstName(){
+        Assertions.assertEquals("", "");
+    }
+    @Test
+    void validateLastName(){
+        Assertions.assertEquals("", "");
+    }
+    @Test
+    void validateEmployeeID(){
+        Assertions.assertEquals("", "");
+    }
+    @Test
+    void validateZipCode(){
+        Assertions.assertEquals("", "");
+    }
+
+}
