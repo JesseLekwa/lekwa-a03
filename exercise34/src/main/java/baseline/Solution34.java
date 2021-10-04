b94942c8d233f41f87b57ec1362e2a4e8fd845bb
JesseLekwa <jesse.lekwa@gmail.com>
2021-10-03 23:03:24 -0400

    exercise 34 final commit

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,37 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // use an array to store in the existing employees
+        ArrayList<String> employees = new ArrayList<String>
+                (Arrays.asList("John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"));
+
+        // print out original list of 5 people
+        System.out.println("" + employees);
+
+        // read in name to remove from list
+        System.out.println("Enter an employee name to remove: ");
+        String removeEmployee = sc.nextLine();
+
+        // read in the name through the array to remove it from the array
+        employees.remove(removeEmployee);
+
+        // print out new list of employees with removed name
+        System.out.println("");
+
+    }
+
+    public static void printEmployees(ArrayList<String> employees) {
+        System.out.println("There are " + employees.size() + " employees: ");
+        for (String emp : employees) {
+            System.out.println(emp);
+        }
+    }
+}
